421cd22a2d66574f5b70b0aca554d6822f5129fb
Jonathan Slack <slack6000@gmail.com>
2022-06-09 14:31:16 -0500

    8th save

--- server/index.js
+++ server/index.js
@@ -4,6 +4,17 @@ const path = require('path')
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '7f209f3a1c3641c68222becb4932713c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
